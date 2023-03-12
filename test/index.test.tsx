d57971560237ede46d1a2e4a94888003a7e29d18
derodero24 <k.itsh20524abc@gmail.com>
2023-03-13 01:34:01 +0900

    fix test code

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import { Thing } from '../src/index';
+import { Bitcoin } from '../src/index';
 
-// describe('Thing', () => {
-//   it('renders without crashing', () => {
-//     const root = ReactDOM.createRoot(document.createElement('div'));
-//     root.render(<Thing />);
-//     root.unmount();
-//   });
-// });
+describe('Icons', () => {
+  it('renders without crashing', () => {
+    const root = ReactDOM.createRoot(document.createElement('div'));
+    root.render(<Bitcoin />);
+    root.unmount();
+  });
+});
