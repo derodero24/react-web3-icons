e68135e0a60b41687455687e2854d09bd46361cb
derodero24 <k.itsh20524abc@gmail.com>
2023-03-14 02:17:39 +0900

    fix test

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Bitcoin } from '../src/index';
+import { Btc } from '../src/index';
 
 describe('Icons', () => {
   it('renders without crashing', () => {
     const root = ReactDOM.createRoot(document.createElement('div'));
-    root.render(<Bitcoin />);
+    root.render(<Btc />);
     root.unmount();
   });
 });
