01d79d0d43f73efd18cfff494e79059eaf4adaad
derodero24 <k.itsh20524abc@gmail.com>
2023-03-14 23:00:06 +0900

    add ImmutableX

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -5,6 +5,7 @@ export * from './BinanceSmartChain';
 export * from './Bitcoin';
 export * from './Cardano';
 export * from './Ethereum';
+export * from './ImmutableX';
 export * from './Polkadot';
 export * from './Polygon';
 export * from './Solana';
