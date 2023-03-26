23ecba0b1ffe88a7ec26d0e035e99ffd697823b9
derodero24 <k.itsh20524abc@gmail.com>
2023-03-26 23:21:12 +0900

    add CardanoMono2, AdaMono2

--- src/coin/Eth.tsx
+++ src/coin/Eth.tsx
@@ -1,10 +1,1 @@
-import { Ethereum, EthereumMono } from '../chain/Ethereum';
-
-import type { IconProps } from '../lib';
-
-export function Eth(props: IconProps) {
-  return <Ethereum {...props} />;
-}
-export function EthMono(props: IconProps) {
-  return <EthereumMono {...props} />;
-}
+export { Ethereum as Eth, EthereumMono as EthMono } from '../chain/Ethereum';
