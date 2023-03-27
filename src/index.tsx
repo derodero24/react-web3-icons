57ef614f6dee87aad019dd0d1fa6553556c37383
derodero24 <k.itsh20524abc@gmail.com>
2023-03-27 17:55:01 +0900

    add Ens

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export * from './chain';
 export * from './coin';
 export * from './dex';
+export * from './domain';
 export * from './exchange';
 export * from './library';
 export * from './marketplace';
