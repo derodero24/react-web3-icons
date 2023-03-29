b5ed6ccb160caf51f29057652a8b87a56d13ba77
derodero24 <k.itsh20524abc@gmail.com>
2023-03-29 20:40:20 +0900

    add Ltc

---
+++ src/coin/Ltc.tsx
@@ -0,0 +1,35 @@
+import type { IconProps } from '../lib';
+
+export function Ltc(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 82.6 82.6"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      {props.title && <title>{props.title}</title>}
+      <circle cx="41.3" cy="41.3" r="36.83" fill="#fff" />
+      <path
+        d="M41.3,0A41.3,41.3,0,1,0,82.6,41.3h0A41.18,41.18,0,0,0,41.54,0ZM42,42.7,37.7,57.2h23a1.16,1.16,0,0,1,1.2,1.12v.38l-2,6.9a1.49,1.49,0,0,1-1.5,1.1H23.2l5.9-20.1-6.6,2L24,44l6.6-2,8.3-28.2a1.51,1.51,0,0,1,1.5-1.1h8.9a1.16,1.16,0,0,1,1.2,1.12v.38L43.5,38l6.6-2-1.4,4.8Z"
+        fill="#345d9d"
+      />
+    </svg>
+  );
+}
+export function LtcMono(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 82.6 82.6"
+      width="1em"
+      height="1em"
+      fill="CurrentColor"
+      {...props}
+    >
+      {props.title && <title>{props.title}</title>}
+      <path d="M41.3,0A41.3,41.3,0,1,0,82.6,41.3h0A41.18,41.18,0,0,0,41.54,0ZM42,42.7,37.7,57.2h23a1.16,1.16,0,0,1,1.2,1.12v.38l-2,6.9a1.49,1.49,0,0,1-1.5,1.1H23.2l5.9-20.1-6.6,2L24,44l6.6-2,8.3-28.2a1.51,1.51,0,0,1,1.5-1.1h8.9a1.16,1.16,0,0,1,1.2,1.12v.38L43.5,38l6.6-2-1.4,4.8Z" />
+    </svg>
+  );
+}
