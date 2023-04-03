64544df56d8b8c24ee2993f0d921ab2f555e7ab1
derodero24 <k.itsh20524abc@gmail.com>
2023-04-03 17:14:32 +0900

    add Bitstamp

--- src/exchange/Coinbase.tsx
+++ src/exchange/Coinbase.tsx
@@ -66,10 +66,10 @@ export function CoinbaseMono(props: IconProps) {
       {props.title && <title>{props.title}</title>}
       <defs>
         <mask id="cbem-a">
-          <rect width="100%" height="100%" fill="white" />
+          <rect width="100%" height="100%" fill="#fff" />
           <path
             d="M1250.4 1689.5c-242.8 0-439.4-196.7-439.4-439.5s196.7-439.4 439.4-439.4c217.5 0 398.1 158.6 432.9 366.2H2126c-37.4-451.2-414.9-805.7-875.6-805.7-485.2 0-878.9 393.7-878.9 878.9s393.7 878.9 878.9 878.9c460.7 0 838.3-354.5 875.6-805.7h-443.1c-34.8 207.7-215 366.3-432.5 366.3h0z"
-            fill="black"
+            fill="#000"
           />
         </mask>
       </defs>
--- src/exchange/index.ts
+++ src/exchange/index.ts
@@ -1,5 +1,6 @@
 export * from './Binance';
 export * from './Bitfinex';
+export * from './Bitstamp';
 export * from './Bybit';
 export * from './Coinbase';
 export * from './Kraken';
