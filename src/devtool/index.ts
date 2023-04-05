656153ea37be6d0e3b7e0dc69c35ec36fe52a4bf
derodero24 <k.itsh20524abc@gmail.com>
2023-04-05 16:17:33 +0900

    add Remix

--- src/devtool/index.ts
+++ src/devtool/index.ts
@@ -3,5 +3,6 @@ export * from './EthersJs';
 export * from './Ganache';
 export * from './Hardhat';
 export * from './OpenZeppelin';
+export * from './Remix';
 export * from './Truffle';
 export * from './Web3Js';
