1a9636e1048cd7bdf549e299500cb9290b75b896
derodero24 <shintaro@payn.io>
2026-03-08 15:02:44 +0900

    fix(devtool): ChainlinkMono mask works correctly with any currentColor
    - Moved <mask> into <defs> for SVG spec conformance
    - Added explicit fill="#000" to the inner hexagon cutout path
    
    Previously the cutout path inherited currentColor from the SVG
    element. With light or white currentColor the mask failed to cut,
    rendering a solid hexagon instead of a ring.
    
    Closes #468

--- src/devtool/Chainlink.tsx
+++ src/devtool/Chainlink.tsx
@@ -18,13 +18,17 @@ export const ChainlinkMono = createIcon(
   '0 0 37.8 43.6',
   _id => (
     <>
-      <mask id={`${_id}-chl-a`}>
-        <rect width="100%" height="100%" fill="#fff" />
-        <path d="M8 28.1V15.5l10.9-6.3 10.9 6.3v12.6l-10.9 6.3L8 28.1z" />
-      </mask>
+      <defs>
+        <mask id={`${_id}-chl-a`}>
+          <rect width="100%" height="100%" fill="#fff" />
+          <path
+            d="M8 28.1V15.5l10.9-6.3 10.9 6.3v12.6l-10.9 6.3L8 28.1z"
+            fill="#000"
+          />
+        </mask>
+      </defs>
       <path
         d="M18.9 0l-4 2.3L4 8.6l-4 2.3V32.7L4 35l11 6.3 4 2.3 4-2.3L33.8 35l4-2.3V10.9l-4-2.3-10.9-6.3-4-2.3z"
-        fill="currentColor"
         mask={`url(#${_id}-chl-a)`}
       />
     </>
