3d56dafce8ae6af1adf7a848352015894e8b40a8
derodero24 <shintaro@payn.io>
2026-03-11 14:55:36 +0900

    feat(dex): add Odos and ParaSwap icons
    Add colored and mono variants for both DEX protocols.
    SVG paths sourced from @web3icons/react.

---
+++ .changeset/add-odos-paraswap.md
@@ -0,0 +1,5 @@
+---
+"react-web3-icons": minor
+---
+
+Add Odos and ParaSwap DEX icons
---
+++ src/dex/ParaSwap.tsx
@@ -0,0 +1,23 @@
+import { createIcon } from '../utils';
+
+const paraSwapContent = () => (
+  <>
+    <path d="M21 19.2 12.675 4.8 4.35 19.2z" />
+    <path d="M6.825 11.55 10.65 4.8H3z" />
+    <path d="M17.6 17.294H7.75l4.925-8.52z" />
+  </>
+);
+
+export const ParaSwap = createIcon(
+  'ParaSwap',
+  '0 0 24 24',
+  paraSwapContent,
+  '#2669F5',
+);
+
+export const ParaSwapMono = createIcon(
+  'ParaSwapMono',
+  '0 0 24 24',
+  paraSwapContent,
+  'currentColor',
+);
